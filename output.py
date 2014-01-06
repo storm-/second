845fc5d539d678d9594b8b50defd60a67a2d197a
storm- <storm.exe@gmail.com>
2014-01-06 23:14:33 +0000

    Checking the weather.

--- output.py
+++ output.py
@@ -1,5 +1,10 @@
 #this is just some code for an example 
 #when working with github
+
+# Assumes that the package 'weather-util' has been installed.
+#   sudo apt-get install weather-util
+#   weather -iKAUS
+
 import subprocess
 
 weather=subprocess.check_output(["weather", "-iKAUS"])
